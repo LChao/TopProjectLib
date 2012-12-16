8fe3bed807c317f5f7bb95c56c09a4005879c21c
Administrator <Administrator@Win7-PC>
2012-12-16 23:15:57 +0800

    bug 后退按钮

--- src/com/tianxia/lib/baseworld/activity/SettingAboutActivity.java
+++ src/com/tianxia/lib/baseworld/activity/SettingAboutActivity.java
@@ -6,41 +6,43 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.WebView;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.tianxia.lib.baseworld.R;
 
-public class SettingAboutActivity extends BaseActivity{
+public class SettingAboutActivity extends BaseActivity {
 
-    private Button mAppBackButton = null;
+	private ImageView mAppBackButton = null;
 
-    private TextView mSettingAboutVersionTextView;
-    private WebView mSettingAboutLinkWebView;
+	private TextView mSettingAboutVersionTextView;
+	private WebView mSettingAboutLinkWebView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.setting_about_activity);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.setting_about_activity);
 
-        mAppBackButton  = (Button) findViewById(R.id.app_back);
-        mAppBackButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+		mAppBackButton = (ImageView) findViewById(R.id.app_back);
+		mAppBackButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				onBackPressed();
+			}
+		});
 
-        mSettingAboutVersionTextView = (TextView) findViewById(R.id.setting_about_version);
-        PackageInfo pinfo;
-        try {
-            pinfo = this.getPackageManager().getPackageInfo(this.getPackageName(), PackageManager.GET_CONFIGURATIONS);
-            mSettingAboutVersionTextView.setText(pinfo.versionName);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+		mSettingAboutVersionTextView = (TextView) findViewById(R.id.setting_about_version);
+		PackageInfo pinfo;
+		try {
+			pinfo = this.getPackageManager().getPackageInfo(
+					this.getPackageName(), PackageManager.GET_CONFIGURATIONS);
+			mSettingAboutVersionTextView.setText(pinfo.versionName);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
-        mSettingAboutLinkWebView = (WebView) findViewById(R.id.setting_about_link);
-        mSettingAboutLinkWebView.loadUrl("file:///android_asset/setting_about_link.html");
-    }
+		mSettingAboutLinkWebView = (WebView) findViewById(R.id.setting_about_link);
+		mSettingAboutLinkWebView
+				.loadUrl("file:///android_asset/setting_about_link.html");
+	}
 }
