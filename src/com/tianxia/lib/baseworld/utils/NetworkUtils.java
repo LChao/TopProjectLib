b3426ee96122ae7a0f7bde75f55da5acb365abcb
Administrator <Administrator@XP-201207110838>
2012-12-31 17:04:42 +0800

    框架网络状态修复

--- src/com/tianxia/lib/baseworld/utils/NetworkUtils.java
+++ src/com/tianxia/lib/baseworld/utils/NetworkUtils.java
@@ -2,34 +2,46 @@ package com.tianxia.lib.baseworld.utils;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo.State;
+import android.net.NetworkInfo;
+import android.widget.Toast;
 
 public class NetworkUtils {
 
-    public static final int NETWORN_NONE = 0;
-    public static final int NETWORN_WIFI = 1;
-    public static final int NETWORN_MOBILE = 2;
+	public static final int NETWORN_NONE = 0;
+	public static final int NETWORN_WIFI = 1;
+	public static final int NETWORN_MOBILE = 2;
 
-    public static int getNetworkState(Context context){
-        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+	public static int getNetworkState(Context context) {
+		ConnectivityManager connManager = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        //Wifi
-        State state = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState();
-        if(state == State.CONNECTED||state == State.CONNECTING){
-            return NETWORN_WIFI;
-        }
-
-        //3G
-        try {
-        	state = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState();
-            if(state == State.CONNECTED||state == State.CONNECTING){
-                return NETWORN_MOBILE;
-            }
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
+		// Wifi
+		// State state =
+		// connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+		// .getState();
+		// if (state == State.CONNECTED || state == State.CONNECTING) {
+		// return NETWORN_WIFI;
+		// }
+		NetworkInfo mMobileNetworkInfo = connManager
+				.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+		if (mMobileNetworkInfo != null && mMobileNetworkInfo.isAvailable()) {
+			return NETWORN_WIFI;
 		}
-        
-        return NETWORN_NONE;
-    }
+
+		// 3G
+		// state = connManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+		// .getState();
+		// if (state == State.CONNECTED || state == State.CONNECTING) {
+		// return NETWORN_MOBILE;
+		// }
+		mMobileNetworkInfo = connManager
+				.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+		if (mMobileNetworkInfo != null && mMobileNetworkInfo.isAvailable()
+				&& mMobileNetworkInfo.isConnected()) {
+			Toast.makeText(context, "移动网络链接可用", 0).show();
+			return NETWORN_WIFI;
+		}
+
+		return NETWORN_NONE;
+	}
 }
